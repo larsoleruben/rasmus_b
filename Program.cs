3f92d96b6ae835e14aadf3bcf082b94e09459fc9
Lars Ole Christoffersen <2634802+larsoleruben@users.noreply.github.com>
2022-04-27 16:02:29 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace atomics
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+             var prefixes = new List<string>() { "http://*:8888/" };
+             AtomicsListener(prefixes);
+        }
+
+        public static void AtomicsListener(List<string> prefixes)
+        {
+            if (prefixes == null || prefixes.Count == 0)
+                throw new ArgumentException("prefixes");
+
+            HttpListener listener = new HttpListener();
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+           while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                string documentContents;
+                using (Stream receiveStream = request.InputStream)
+                {
+                    using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                    {
+                        documentContents = readStream.ReadToEnd();
+                    }
+                }
+                Console.WriteLine($"Recived request for {request.Url}");
+                Console.WriteLine(documentContents);
+
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            listener.Stop();
+        }
+    }
+}
